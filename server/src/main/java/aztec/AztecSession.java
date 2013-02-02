3382d06e49384698abb755be27c008340e4a9b37
Charlie Groves <charlie.groves@gmail.com>
2013-02-01 20:50:14 -0500

    Remove unused session end listener

--- server/src/main/java/aztec/AztecSession.java
+++ server/src/main/java/aztec/AztecSession.java
@@ -9,10 +9,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class AztecSession extends PresentsSession {
-    public interface SessionEndListener
-    {
-        void sessionEnded(AztecSession session);
-    }
 
     @Override
     protected void sessionConnectionClosed ()
@@ -21,27 +17,4 @@ public class AztecSession extends PresentsSession {
         // Since there's no place state in aztec, we don't keep disconnected sessions
         endSession();
     }
-
-    @Override
-    protected void sessionDidEnd ()
-    {
-        super.sessionDidEnd();
-        for (SessionEndListener listener : _endListeners.toArray(new SessionEndListener[0])) {
-            listener.sessionEnded(this);
-        }
-    }
-
-    public void addSessionEndListener (SessionEndListener listener)
-    {
-        _endListeners.add(listener);
-    }
-
-    public void removeSessionEndListener (SessionEndListener sessionEndListener)
-    {
-        _endListeners.remove(sessionEndListener);
-    }
-
-    @Inject AztecMatchmaker _matchmaker;
-
-    protected final List<SessionEndListener> _endListeners = Collections.<SessionEndListener>synchronizedList(Lists.<SessionEndListener>newArrayList());
 }
